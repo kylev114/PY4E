f4fba611edef6538bb025cfd7541db02e42e9d2a
lolmanyaa <kylevo114@gmail.com>
2023-02-07 22:20:10 -0500

    add chp 14 notes

--- Chapter 14 Object-oriented Programming/14.3_reviewFunctions.py
+++ Chapter 14 Object-oriented Programming/14.3_reviewFunctions.py
@@ -1,1 +1,22 @@
-# Refer to functions (chapter 4) and the BeautifulSoup notes ()
+# Refer to functions (chapter 4). At its most basic functions 
+# takes inputs and returns outputs. The input/output are on the
+# "outside" which is seperated from the function code "inside."
+
+# Refer to BeautifulSoup program (12.8) and (14.4) figure.
+# This program takes a url input and outputs the href tags. 
+# Within the program are several process;
+
+    # URL is read into a string and then passed into urllib to retrieve the data
+    # from the web. The urllib library uses the socket library to make the actual
+    # network connection to retrieve the data. Urllib returns a string which is passed
+    # to BeautifulSoup for parsing. BeautifulSoup makes use of the object
+    # html.parser1 and returns an object. tags() method is called on the returned
+    # object that returns a dictionary of tag objects. Thet tags are looped through  
+    # the get() method is called for each tag to print out the href attribute.
+
+# This movement of interacting objects and infomration is contained 
+# within the program, like the black box of a function. 
+
+# With O-OP, complex systems can br broken into parts. The parts of 
+# BeautifulSoup library is ignored and the using the program only 
+# requires knowledge about the string object inputs and outputs.
---
+++ Chapter 14 Object-oriented Programming/14.5_functionVsObject.py
@@ -0,0 +1,6 @@
+# At a basic level, defining a function allows us to store a bit of code and
+# give it a name and then later invoke that code using the name of the function.
+# 
+#  an object is simply some code plus data structures that are smaller
+# than a whole program. Defining a function allows us to store a bit of code and
+# give it a name and then later invoke that code using the name of the function.
