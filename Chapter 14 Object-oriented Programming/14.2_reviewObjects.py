b370d43c87068a2db6ace2aa7dab1139985f3f17
lolmanyaa <kylevo114@gmail.com>
2023-02-09 09:32:01 -0500

    update chp 14 notes

--- Chapter 14 Object-oriented Programming/14.2_reviewObjects.py
+++ Chapter 14 Object-oriented Programming/14.2_reviewObjects.py
@@ -3,6 +3,13 @@
 
 stuff = list()                      # constructs an object of type list
 
+# Remeber that we can take a use dir() function to return list of
+# different methods an object has:
+
+print(dir(stuff))
+
+# The following are examples of calling methods to an object:
+
 stuff.append('python')              # calls the append() method on object
 stuff.append('chuck')               # calls the append() method on object
 stuff.sort()                        # calls the sort() method on object
@@ -10,8 +17,5 @@ print (stuff[0])                    # retrieves item at pssition 0
 print (stuff.__getitem__(0))        # cals __getitem__() method with parameter 0
 print (list.__getitem__(stuff,0))   # alternative way to use __getitem__() specifying list object
 
-# Remeber that we can take a use dir() function to return list of
-# different methods an object has:
 
-print(dir(stuff))
 
--- Chapter 14 Object-oriented Programming/14.3_reviewFunctions.py
+++ Chapter 14 Object-oriented Programming/14.3_reviewFunctions.py
@@ -1,10 +1,10 @@
-# Refer to functions (chapter 4). At its most basic functions 
+# Refer to functions (chapter 4). At its most basic, functions 
 # takes inputs and returns outputs. The input/output are on the
 # "outside" which is seperated from the function code "inside."
 
 # Refer to BeautifulSoup program (12.8) and (14.4) figure.
 # This program takes a url input and outputs the href tags. 
-# Within the program are several process;
+# Within the program are several processes:
 
     # URL is read into a string and then passed into urllib to retrieve the data
     # from the web. The urllib library uses the socket library to make the actual
@@ -17,6 +17,12 @@
 # This movement of interacting objects and infomration is contained 
 # within the program, like the black box of a function. 
 
-# With O-OP, complex systems can br broken into parts. The parts of 
-# BeautifulSoup library is ignored and the using the program only 
+# With O-OP, complex systems can be broken into parts. The parts of 
+# BeautifulSoup library is ignored and using the program only 
 # requires knowledge about the string object inputs and outputs.
+
+# The creators of BeautifulSoup library did not create their library
+# to ensure programmers can create a href parser. They created the object
+# that can be incorporated into programs like 12.8 without requiring
+# the resources and time to understand what is happening on the "inside"
+# of their program. 
